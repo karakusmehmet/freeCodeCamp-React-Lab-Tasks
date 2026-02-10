597107c05fb5083a05907b5f8c0a6804261dfd42
Mehmet Karakus <karakusmehmet9833@gmail.com>
2026-02-10 06:33:28 +0300

    project has been done

--- todo-app-w-typescript/src/components/Todo.tsx
+++ todo-app-w-typescript/src/components/Todo.tsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoMdRemoveCircleOutline } from "react-icons/io";
 import { FaCheck } from "react-icons/fa";
 import { FaRegEdit } from "react-icons/fa";
 import type { TodoType } from '../types/Types';
+import {removeTodoById,updateTodo} from '../redux/todoSlice'
+import { useDispatch } from 'react-redux';
 
 interface TodoProps {
   todoProps: TodoType
 }
+
 const Todo = ({todoProps} :TodoProps ) => {
-  const {content} = todoProps;
+  const {id,content} = todoProps;
+
+  const [editable,setEditable] = useState<boolean>(false);
+  const [newTodo, setNewTodo] = useState<string>(content)
+  const dispatch = useDispatch();
+
+  const handleUpdate = () :void => {
+      const payload : TodoType = {
+        id : id,
+        content: newTodo,
+      }
+
+      dispatch(updateTodo(payload))
+      setEditable(false);
+  }
+
   return (
     <div style={{display:"flex", alignItems:"center", justifyContent: "space-between", border:"1px solid lightgray", padding: "16px", marginTop:"25px", borderRadius: "5px"}}>
-        <div>{content}</div>
+        {editable ? <input style= {{border:"none", borderBottom:"1px solid lightgrey", outline:"none", width:"400px" }} value={newTodo} onChange={(e:React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => setNewTodo(e.target.value) }/> : <div>{content}</div>}
         <div className='icons'>
-            <IoMdRemoveCircleOutline style={{marginRight:"5px"}} />
-            <FaRegEdit />
-            <FaCheck></FaCheck>
+            <IoMdRemoveCircleOutline onClick={() => dispatch(removeTodoById(id))} style={{marginRight:"5px"}} />
+            {editable ? <FaCheck onClick={handleUpdate}/>: <FaRegEdit onClick={() => setEditable(true)} /> }
+            
         </div>
     </div>
   )
