74dea37d8c7765fcbe855e8491f41ef99ddb9887
karakusmehmet <karakusmehmet9833@gmail.com>
2026-03-12 13:58:53 +0300

    shopping cart project is done

--- shopping-cart-managament/src/App.tsx
+++ shopping-cart-managament/src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Cart from "./components/Cart"
+import Header from "./components/Header"
+import ProductList from "./components/ProductList"
+import Footer from "./components/Footer"
 
-const App = () => {
-  return (
-    <div>App</div>
+import { useState } from "react"
+
+function App() {
+  const [viewCart, setViewCart] = useState<boolean>(false)
+
+  const pageContent = viewCart ? <Cart /> : <ProductList />
+
+  const content = (
+    <>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart} />
+    </>
   )
+
+  return content
 }
 
 export default App
--- shopping-cart-managament/src/main.tsx
+++ shopping-cart-managament/src/main.tsx
@@ -2,6 +2,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { CartProvider } from './context/CartProvider.tsx'
+import { ProductsProvider } from './context/ProductsProvider.tsx'
+
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <ProductsProvider>
+        <CartProvider>
+            <App />
+        </CartProvider>
+    </ProductsProvider>
+    
+   
 )
